d4f0c43823bae4ce14abd6b0a190763e476dfe2f
Danva16 <danieladannava111116@gmail.com>
2023-06-03 01:17:33 -0600

    Hacemos funcional nuestro formulario

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
 let popup = document.querySelector('.profile-popup');
 let editButton = document.querySelector('.button__action_edit');
 let closeButton = popup.querySelector('.button__action_close');
+let submitButton = popup.querySelector('.button__action_create');
+let nameInput = popup.querySelector('input[placeholder="Nombre"]');
+let employmentInput = popup.querySelector('input[placeholder="Acerca de mí"]');
 
 function openPopup() {
+  let name = document.querySelector('.profile__username');
+  let employment = document.querySelector('.profile__useremployment');
+
+  nameInput.value = name.textContent;
+  employmentInput.value = employment.textContent;
+
   popup.classList.remove('profile-popup_mode_disabled');
 }
 
@@ -10,5 +19,21 @@ function closePopup() {
   popup.classList.add('profile-popup_mode_disabled');
 }
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameOutput = document.querySelector('.profile__username');
+  let employmentOutput = document.querySelector('.profile__useremployment');
+
+  let nameValue = nameInput.value;
+  let employmentValue = employmentInput.value;
+
+  nameOutput.textContent = nameValue;
+  employmentOutput.textContent = employmentValue;
+
+  closePopup();
+}
+
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+submitButton.addEventListener('click', handleProfileFormSubmit);
