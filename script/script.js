b80e5a5377252b99cfd77ed860c5f8a6626f9cf8
Danva16 <danieladannava111116@gmail.com>
2023-06-16 01:53:41 -0600

    feat: agregar las cards con javaScript y las nuevas agregarlas al array

--- script/script.js
+++ script/script.js
@@ -52,6 +52,37 @@ function addImage(titleValue, imageValue) {
   places.append(placeElement);
 }
 
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+  }
+];
+
+initialCards.forEach((card) => {
+  addImage(card.name, card.link);
+});
+
 function handleImageFormSubmit(evt) {
   evt.preventDefault();
 
@@ -59,6 +90,7 @@ function handleImageFormSubmit(evt) {
   const imageValue = imageInput.value;
 
   addImage(titleValue, imageValue);
+  initialCards.push({name: titleValue, link: imageValue});
   closePopup();
 }
 
