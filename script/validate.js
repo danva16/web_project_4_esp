c9a109e7ac30d9b409a4649c63a0e3eeaf8e7ac5
Danva16 <danieladannava111116@gmail.com>
2023-07-06 18:57:42 -0600

    feat: funcion que comprobara validez del input

--- script/validate.js
+++ script/validate.js
@@ -13,3 +13,12 @@ const hideInputError = (formElement, inputElement) => {
   errorElement.classList.remove("form__input-error_active");
   errorElement.textContent = '';
 };
+
+const checkInputValidity = (formElement, inputElement) => {
+  if(!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  }
+  else {
+    hideInputError(formElement, inputElement);
+  }
+};
