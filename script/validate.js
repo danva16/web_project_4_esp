e1d95220c2b0756049e99536b54d01e1da8e176d
Danva16 <danieladannava111116@gmail.com>
2023-09-20 15:29:41 -0600

    feater: limpieza del codigo

--- script/validate.js
+++ script/validate.js
@@ -1,76 +1,3 @@
-/*const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-
-  inputElement.classList.add("form__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
-  errorElement.textContent = '';
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if(!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  }
-  else {
-    hideInputError(formElement, inputElement);
-  }
-
-  const inputList = Array.from(document.querySelectorAll(".form__input"));
-  const buttonElement = formElement.querySelector(".button_action_create");
-  toggleButtonState(inputList, buttonElement);
-};
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  })
-};
-
-const toggleButtonState = (inputList, buttonElement) => {
-  if(hasInvalidInput(inputList)) {
-    buttonElement.classList.add("button_action_create-inactive");
-  }
-  else {
-    buttonElement.classList.remove("button_action_create-inactive");
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
-  const buttonElement = formElement.querySelector(".button_action_create");
-  toggleButtonState(inputList, buttonElement);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", function() {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".form"));
-  formList.forEach((form) => {
-    form.addEventListener("submit", function(evt) {
-      evt.preventDefault();
-    })
-  })
-
-  const fieldsetList = Array.from(document.querySelectorAll(".form__set"));
-
-  fieldsetList.forEach((fieldset) => {
-    setEventListeners(fieldset);
-  });
-};
-
-enableValidation();*/
-
 class FormValidator {
   constructor(config, formElement) {
     this._config = config;
@@ -125,7 +52,7 @@ class FormValidator {
     });
   }
 
-  _enableValidation() {
+  enableValidation() {
     this._formElement.addEventListener("submit", evt => {
       evt.preventDefault();
     });
@@ -138,9 +65,9 @@ const formValidatorConfig = {
   formSelector: ".form",
   inputSelector: ".form__input",
   submitButtonSelector: ".button_action_create",
-  inactiveButtonClass: ".button_action_create-inactive",
-  inputErrorClass: ".form__input_type_error",
-  errorClass: ".form__input-error_active"
+  inactiveButtonClass: "button_action_create-inactive",
+  inputErrorClass: "form__input-error",
+  errorClass: "form__input-error_active"
 }
 
 const formElement = document.querySelector(".form");
